cccd380b1fc0ef563d071937274e96d9b228e94a
thahip <gavinhyamdesign@hotmail.co.uk>
2016-04-14 12:06:40 +0100

    form code and styling

--- form.php
+++ form.php
@@ -1,8 +1,21 @@
-<html>
-    <head>
-        
-    </head>
-    <body>
-        <h1>Form Title</h1>
-    </body>
-</html>
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: form demo'; 
+    $to = 'gavinhyamdesign@hotmail.co.uk'; 
+    $subject = 'This is from a form';
+    $human = $_POST['human'];
+                
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+                    
+    if ($_POST['submit'] && $human == '4') {                 
+        if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Your message has been sent!</p>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    } 
+    } else if (isset($_POST['submit']) && $human != '4') {
+    echo '<p>You answered the anti-spam question incorrectly!</p>';
+    }
+?>
--- index.php
+++ index.php
@@ -1,8 +1,42 @@
+<!DOCTYPE HTML>
 <html>
-    <head>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Contact Form</title>
+
+<link rel="stylesheet" href="css/form.css">
+</head>
+
+<body>
+
+    <header class="body">
+    </header>
+
+    <section class="body">
+
+    <form method="post" action="form.php">
         
-    </head>
-    <body>
-        <h1>Page Title</h1>
-    </body>
+    <label>Name</label>
+    <input name="name" placeholder="Type Here">
+            
+    <label>Email</label>
+    <input name="email" type="email" placeholder="Type Here">
+            
+    <label>Message</label>
+    <textarea name="message" placeholder="Type Here"></textarea>
+    
+    <label>*What is 2+2? (Anti-spam)</label>
+    <input name="human" placeholder="Type Here">
+    <input id="submit" name="submit" type="submit" value="Submit">
+        
+    </form>
+
+    </section>
+
+    <footer class="body">
+    </footer>
+
+</body>
+
 </html>
